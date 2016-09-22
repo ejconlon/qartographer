d4ad58f971d12d6e19ac0f2c54e9b1e80f333344
Eric Conlon <ejconlon@gmail.com>
2016-09-22 08:39:31 -0700

    client

--- src/Qartographer/Client/Http.hs
+++ src/Qartographer/Client/Http.hs
@@ -1,2 +1,52 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Qartographer.Client.Http where
 
+import qualified Data.ByteString.Lazy as BL
+import Data.ByteString.Lazy (ByteString)
+import Control.Monad.IO.Class
+import Control.Monad.Reader
+import Network.HTTP.Client
+import Network.Wreq (defaults)
+import Network.Wreq.Session
+import Network.Wreq.Types (Options(..), Postable(..))
+import System.FilePath.Posix ((</>))
+
+data ClientEnv = ClientEnv
+  { _clientEnvHostPort :: String
+  , _clientEnvSession :: Session
+  } deriving (Show)
+
+localClient :: Int -> Session -> ClientEnv
+localClient port = ClientEnv ("http://localhost:" ++ (show port))
+
+makeUrl :: MonadReader ClientEnv m => FilePath -> m String
+makeUrl path = do
+  hostPort <- asks _clientEnvHostPort
+  return $ hostPort </> path
+
+clientGet :: (MonadReader ClientEnv m, MonadIO m) => FilePath -> m (Response ByteString)
+clientGet path = clientGetWith defaults path
+
+clientGetWith :: (MonadReader ClientEnv m, MonadIO m) => Options -> FilePath -> m (Response ByteString)
+clientGetWith opts path = do
+  sess <- asks _clientEnvSession
+  url <- makeUrl path
+  liftIO $ getWith opts sess url
+
+clientPost :: (MonadReader ClientEnv m, MonadIO m, Postable a) => FilePath -> a -> m (Response ByteString)
+clientPost path payload = clientPostWith defaults path payload
+
+clientPostWith :: (MonadReader ClientEnv m, MonadIO m, Postable a) => Options -> FilePath -> a -> m (Response ByteString)
+clientPostWith opts path payload = do
+  sess <- asks _clientEnvSession
+  url <- makeUrl path
+  liftIO $ postWith opts sess url payload
+
+newtype Client a = Client
+  { unClient :: ReaderT ClientEnv IO a
+  } deriving (Functor, Applicative, Monad, MonadReader ClientEnv, MonadIO)
+
+runClient :: Client a -> ClientEnv -> IO a
+runClient = runReaderT . unClient
--- src/Qartographer/Integration/Http.hs
+++ src/Qartographer/Integration/Http.hs
@@ -4,38 +4,24 @@ module Qartographer.Integration.Http where
 
 import Control.Concurrent
 import Control.Exception
-import Control.Monad.IO.Class
-import Control.Monad.Reader
 import Network.HTTP.Client
 import Network.Wai
 import Network.Wai.Handler.Warp as W
 import Network.Wreq.Session
+import Qartographer.Client.Http
 import Qartographer.Server.Http
 
-data IntegrationEnv = IntegrationEnv
-  { _integrationEnvPort :: Int
-  , _integrationEnvSession :: Session
-  } deriving (Show)
+type WithClient a = Int -> Client a -> IO a
 
-newtype Integration a = Integration
-  { unIntegration :: ReaderT IntegrationEnv IO a
-  } deriving (Functor, Applicative, Monad, MonadReader IntegrationEnv, MonadIO)
-
-runIntegration :: Integration a -> Int -> Session -> IO a
-runIntegration integration port sess =
-  runReaderT (unIntegration integration) (IntegrationEnv port sess)
-
-type WithIntegration a = Int -> Integration a -> IO a
-
-withIntegration :: WithIntegration a
-withIntegration port integration =
-  let callback = runIntegration integration port
+withClient :: WithClient a
+withClient port client =
+  let callback = runClient client . localClient port
       cookieJar = createCookieJar []
       manager = defaultManagerSettings
   in withSessionControl (Just cookieJar) manager callback
 
-runApplication :: Int -> Application -> Integration a -> IO a
-runApplication port app integration = do
+runApplication :: Int -> Application -> Client a -> IO a
+runApplication port app client = do
   bracket (forkIO $ W.run port app) (killThread) $ \_ -> do
     -- threadDelay 1000 
-    withIntegration port integration
+    withClient port client
